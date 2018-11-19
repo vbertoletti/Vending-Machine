1dc8bcb56384b1aebcd18e67bae2efff11a392d4
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-11-18 17:29:35 -0800

    work on tests, functions and readme file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Vending Machine
+# Vending Machine - Project Description :point_down:
+
+A JavaScript class simulating a vending machine. 
+The purpose of this project is to write functions that successfully pass all purposed tests using Jest, there's no UI. 
+
+## Technologies: 
+
+- `JavaScript`;
+- `Jest`: JavaScript testing library for a test-driven development(TDD) approach;
+
+### Applicable Tests :thumbsup:
+
+<img width="714" alt="screen shot 2018-11-18 at 5 06 33 pm" src="https://user-images.githubusercontent.com/40447526/48681276-588ab400-eb56-11e8-9566-d17f73e9ed4f.png">
+
+#### Author: 
+Vanessa Bertoletti.
+
+
+
+
+
+
+
--- _tests_/vending-machine.spec.js
+++ _tests_/vending-machine.spec.js
@@ -8,14 +8,10 @@ describe("VendingMachine", () => {
   });
 
   describe("queryInventory", () => {
-    it("test", () => {
-      expect(machine.test("coke")).toEqual({"stock": 10});
-    });
-  });
-
-  describe("queryInventory", () => {
     it("should return all of the products and their quantaties in the inventory", () => {
-      expect(machine.queryInventory()).toEqual("Coca-Cola: 10, Water: 2, Chocolate Bar: 5, Chips Bag: 12");
+      expect(machine.queryInventory()).toEqual(
+        "Coca-Cola: 10, Water: 2, Chocolate Bar: 5, Chips Bag: 12"
+      );
     });
   });
 
@@ -25,21 +21,65 @@ describe("VendingMachine", () => {
     });
   });
 
-  // describe("resupplyChange", () => {
-  //   it("should re-supply change", () => {
-  //     expect(resupplyChange()).toBe();
-  //   });
-  // });
+  describe("resupplyChange", () => {
+    it("should return the updated total of coins inventory", () => {
+      expect(machine.resupplyChange()).toEqual(400);
+    });
+  });
 
-  // describe("dispenseProductPayment", () => {
-  //   it("should dispense product only upon payment", () => {
-  //     expect(dispenseProductPayment()).toBe();
-  //   });
-  // });
+  describe("dispenseProductPayment", () => {
+    it("should dispense product upon payment with no change", () => {
+      expect(machine.dispenseProductPayment("coke", 2.3)).toEqual({
+        change: "no change needed",
+        item: "Coca-Cola",
+        sold: true
+      });
+    });
 
-  // describe("returnChangeCoins", () => {
-  //   it("should coins change", () => {
-  //     expect(returnChangeCoins()).toBe();
-  //   });
-  // });
+    it("should give correct change if payment is > than the product's price", () => {
+      expect(machine.dispenseProductPayment("coke", 3.0)).toEqual(
+        "Machine gave 70 coins of penny. Change is 0.70"
+      );
+    });
+
+    it("should throw a message if payment is < than the product's price", () => {
+      expect(machine.dispenseProductPayment("coke", 2.0)).toEqual(
+        "you have inserted less money than the product's price"
+      );
+    });
+  });
+
+  describe("queryByCode", () => {
+    it("should prompt the name and price of the product based on its slot code in the machine", () => {
+      expect(machine.queryByCode("B01")).toEqual(
+        "You chose Chocolate Bar and the price is: 3.75"
+      );
+    });
+
+    it("should return null if the slot code doesn't exist", () => {
+      expect(machine.queryByCode("D20")).toEqual(null);
+    });
+  });
+
+  describe("coinsInventory", () => {
+    it("should return status of coins in the inventory", () => {
+      expect(machine.coinsInventory()).toEqual(
+        "penny needs re-supply, nickel is full, dime is full, quarter is full"
+      );
+    });
+  });
+
+  describe("checkProductExist", () => {
+    it("should confirm that item is already in the machine", () => {
+      expect(machine.checkProductExist("Chips Bag")).toEqual(
+        "This item already exists in the machine"
+      );
+    });
+
+    it("should inform there's no such item in the machine", () => {
+      expect(machine.checkProductExist("Doritos")).toEqual(
+        "This item does not exist in the machine"
+      );
+    });
+  });
 });
